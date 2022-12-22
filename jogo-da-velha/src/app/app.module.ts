a956af4a48edc3c0b23f007e20fe07a1ab93d727
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-12-22 15:23:11 -0300

    Criando a interface gráfica do jogo

--- jogo-da-velha/src/app/app.module.ts
+++ jogo-da-velha/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JogoDaVelhaModule } from './jogo-da-velha';
 
@@ -11,7 +10,6 @@ import { JogoDaVelhaModule } from './jogo-da-velha';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     JogoDaVelhaModule
   ],
   providers: [],
